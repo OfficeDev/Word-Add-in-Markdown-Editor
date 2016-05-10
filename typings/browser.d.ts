40b5b77a807e18434b7f31b6bf4c21ddce269677
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-05-09 18:14:37 -0700

    cleaning up

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/lodash/index.d.ts" />
-/// <reference path="browser/ambient/systemjs/index.d.ts" />
