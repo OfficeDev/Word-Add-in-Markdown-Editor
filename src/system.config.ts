66ac9e9955cbb1b28fcf273dae3903ab797a58cc
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-05-10 13:48:23 -0700

    Modifying import of libraries and adding preloading spinner

--- src/system.config.ts
+++ src/system.config.ts
@@ -1,64 +1,129 @@
-var ngVer = '@2.0.0-rc.1';
-
-var mode = {
-    currentMode: 'dev',
-    dev: {
-        'app': 'www/app', // 'dist',
-        'rxjs': 'node_modules/rxjs',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        '@angular': 'node_modules/@angular'
-    },
-    prod: {
-        'app': 'www/app', // 'dist',
-        'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
-        'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api'
-    },
+interface IPackage {
+    name: string,
+    production: string
+    main?: string,
+    development?: string,
+    defaultExtension?: string
 }
 
-var map = mode[mode.currentMode];
+class Configuration {
+    configure() {
+        this.registerAngular2Packages();
 
-var packages = {
-    'app': {
-        main: 'main.js',
-        defaultExtension: 'js'
-    },
-    'rxjs': {
-        defaultExtension: 'js'
-    },
-    'angular2-in-memory-web-api': {
-        defaultExtension: 'js'
+        System.config({
+            map: this.map,
+            packages: this.packages
+        });
+
+        return this.multiImport()
+            .then(null, console.error.bind(console));
     }
-};
 
-var packageNames = [
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/core',
-    '@angular/http',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-    '@angular/router',
-    '@angular/router-deprecated',
-    '@angular/testing',
-    '@angular/upgrade',
-];
+    queueImport(packageName: string) {
+        this.imports.push(packageName);
+        return this;
+    }
 
-// add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
-packageNames.forEach(function (pkgName) {
-    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
-});
+    useDevelopment() {
+        console.info('Importing development libraries from node_modules.')
+        this.devMode = true;
+        return this;
+    }
 
-if (mode.currentMode === 'prod') {
-    // add map entries for angular packages in the form '@angular/common': 'https://npmcdn.com/@angular/common@0.0.0-3?main=browser'
-    packageNames.forEach(function (pkgName) {
-        map[pkgName] = 'https://npmcdn.com/' + pkgName + ngVer;
-    });
+    useProduction() {
+        console.info('Importing production libraries from CDNs.')
+        this.devMode = false;
+        return this;
+    }
+
+    registerLibraries(packages: IPackage[]) {
+        packages.forEach(pkg => {
+            this.packages[pkg.name] = {
+                main: pkg.main || '',
+                defaultExtension: pkg.defaultExtension || 'js'
+            };
+
+            this.map[pkg.name] = this.devMode ? pkg.development || pkg.production : pkg.production;
+        });
+
+        return this;
+    }
+
+    private map = {};
+    private packages = {};
+    private ngVersion = '@2.0.0-rc.1';
+    private devMode = false;
+    private imports = [];
+
+    private angularPackages = [
+        '@angular/common',
+        '@angular/compiler',
+        '@angular/core',
+        '@angular/http',
+        '@angular/platform-browser',
+        '@angular/platform-browser-dynamic',
+        '@angular/router',
+        '@angular/router-deprecated',
+        '@angular/testing',
+        '@angular/upgrade',
+    ];
+
+    private registerAngular2Packages() {
+        var devModeFlag = true;
+
+        this.angularPackages.forEach(pkgName => {
+            // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }           
+            this.packages[pkgName] = {
+                main: 'index.js',
+                defaultExtension: 'js'
+            };
+
+            if (this.devMode) {
+                if (devModeFlag) {
+                    this.map['@angular'] = 'node_modules/@angular';
+                    devModeFlag = false;
+                }
+            }
+            else {
+                // add map entries for angular packages in the form '@angular/common': 'https://npmcdn.com/@angular/common@0.0.0-3?main=browser'                
+                this.angularPackages.forEach(pkgName => {
+                    this.map[pkgName] = 'https://npmcdn.com/' + pkgName + this.ngVersion;
+                });
+            }
+        });
+
+        return this;
+    }
+
+    private multiImport() {
+        var firstPackage = this.imports.splice(0, 1)[0];
+        var promise = System.import(firstPackage);
+        this.imports.forEach(pkg => {
+            promise = promise.then(() => { return System.import(pkg); });
+        });
+
+        return promise;
+    }
 }
 
-System.config({
-    map: map,
-    packages: packages
-});
-
-System.import('app/main')
-    .then(null, console.error.bind(console));
+new Configuration()
+    .useDevelopment()
+    .registerLibraries(<IPackage[]>[
+        {
+            name: 'app',
+            main: 'main.js',
+            production: 'www/app'
+        },
+        {
+            name: 'rxjs',
+            development: 'node_modules/rxjs',
+            production: 'https://npmcdn.com/rxjs@5.0.0-beta.6'
+        },
+        {
+            name: 'angular2-in-memory-web-api',
+            development: 'node_modules/angular2-in-memory-web-api',
+            production: 'https://npmcdn.com/angular2-in-memory-web-api'
+        }
+    ])    
+    .queueImport('app/main')
+    .configure();
