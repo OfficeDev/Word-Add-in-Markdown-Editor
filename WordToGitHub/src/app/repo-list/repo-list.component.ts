b6c08f7a4e10d74ddf2aa03e82de1c9631b81d56
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-06-02 17:50:47 -0700

    adding uma's changes

--- WordToGitHub/src/app/repo-list/repo-list.component.ts
+++ WordToGitHub/src/app/repo-list/repo-list.component.ts
@@ -1,25 +1,42 @@
 import {Component} from '@angular/core';
 import {Router, OnActivate} from '@angular/router';
 import {Path} from '../shared/helpers/utilities';
+import {StorageHelper} from '../shared/helpers/storage.helper';
 import {GithubService, IRepository} from '../shared/services/github.service';
 
 let view = 'repo-list';
 @Component({
-    templateUrl: Path.template(view)
+    templateUrl: Path.template(view),
+    styleUrls: [Path.style(view)]
 })
 
 export class RepoListComponent implements OnActivate {
     repositories: IRepository[];
+    favoriteRepositories: IRepository[];
+    private _storage: StorageHelper<IRepository>;
 
     constructor(
         private _githubService: GithubService,
-        private _router: Router) { }
+        private _router: Router) {
+        this._storage = new StorageHelper<IRepository>("FavoriteRepositories");
+    }
 
     onSelect(item: IRepository) {
         this._router.navigate(['/repo', item.id]);
     }
 
+    onPin(item: IRepository) {
+        this._storage.add(item.id.toString(), item);
+        this.favoriteRepositories = _.values(this._storage.all());
+    }
+
+    onUnpin(item: IRepository) {
+        this._storage.remove(item.id.toString());
+        this.favoriteRepositories = _.values(this._storage.all());
+    }
+
     routerOnActivate() {
+        this.favoriteRepositories = _.values(this._storage.all());
         this._githubService.repos()
             .then(repos => {
                 this.repositories = repos;
