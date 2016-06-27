a653e8ff9bec929004066e2a09ca85489fabfc22
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-06-27 14:29:05 -0700

    updating from dev-merged

--- WordToGitHub/src/app/repo/repo.component.ts
+++ WordToGitHub/src/app/repo/repo.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs/Rx';
+import {Observable, Subject} from 'rxjs/Rx';
 import {Router, RouteSegment, OnActivate} from '@angular/router';
 import {Path, Utils, StorageHelper} from '../shared/helpers';
 import {GithubService, HamburgerService, IRepository, IRepositoryCollection} from '../shared/services';
@@ -13,10 +13,14 @@ let view = 'repo';
 })
 
 export class RepoComponent implements OnActivate {
-    repositories: Observable<IRepository[]>;
+    repositories: IRepository[];
     query: string;
     selectedOrg: string;
     cache: StorageHelper<IRepository>;
+    page: number = 1;
+    pages = new Subject();
+
+
     constructor(
         private _githubService: GithubService,
         private _hamburgerService: HamburgerService,
@@ -30,8 +34,15 @@ export class RepoComponent implements OnActivate {
     }
 
     routerOnActivate(current: RouteSegment) {
+        this.page = 1;
         this.selectedOrg = current.getParam('org');
-        this.repositories = this._githubService.repos(this.selectedOrg, this.selectedOrg === this._githubService.profile.user.login);
+        this._githubService.repos(this.page, this.selectedOrg, this.selectedOrg === this._githubService.profile.user.login)
+            .subscribe(repos => {
+                if (Utils.isEmpty(repos)) {
+                    return;
+                }
+                this.repositories = repos;
+            });
     }
 
     pin(item: IRepository) {
@@ -40,5 +51,27 @@ export class RepoComponent implements OnActivate {
 
     showMenu() {
         this._hamburgerService.showMenu();
+    }
+
+    showNext() {
+        this.page = this.page + 1;
+        this._githubService.repos(this.page, this.selectedOrg, this.selectedOrg === this._githubService.profile.user.login)
+            .subscribe(repos => {
+                if (Utils.isEmpty(repos)) {
+                    this.page = 1;
+                    this.showPrevious();
+                }
+                this.repositories = repos;
+            });
+    }
+
+    showPrevious() {
+        if (this.page > 1) {
+            this.page = this.page - 1;
+        }
+        this._githubService.repos(this.page, this.selectedOrg, this.selectedOrg === this._githubService.profile.user.login)
+            .subscribe(repos => {
+                this.repositories = repos;
+            });
     }
 }
